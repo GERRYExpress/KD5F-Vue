a68658fdf96b6bea50a44b0688e27944094c1857
GERRY Express <krittameth2020@gmail.com>
2026-03-30 18:57:53 +0700

    import the landing page to app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import LandingPage from './components/LandingPage.vue';
 import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
   <Navbar></Navbar>
+  <LandingPage></LandingPage>
 </template>
