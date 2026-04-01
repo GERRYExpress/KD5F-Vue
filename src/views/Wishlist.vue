324e70e0577bc91d1967decc17fba417c8a72608
GERRY Express <krittameth2020@gmail.com>
2026-04-01 14:36:57 +0700

    add checkout(unfinished) and wishlist page

---
+++ src/views/Wishlist.vue
@@ -0,0 +1,60 @@
+<script lang="ts" setup>
+import CartItem from '../components/CartItem.vue';
+const games: Object[] = [
+    {
+        name: "DARK SOULS III",
+        price: 39.99,
+    },
+    {
+        name: "DARK SOULS REMASTERED",
+        price: 29.99,
+    },
+    {
+        name: "DARK SOULS REMASTERED",
+        price: 29.99,
+    },
+    {
+        name: "DARK SOULS REMASTERED",
+        price: 29.99,
+    },
+    {
+        name: "DARK SOULS II",
+        price: 39.99,
+    },
+    {
+        name: "DARK SOULS II",
+        price: 39.99,
+    },
+    {
+        name: "DARK SOULS II",
+        price: 39.99,
+    },
+    {
+        name: "ELDEN RING",
+        price: 49.99,
+    },
+    {
+        name: "ELDEN RING",
+        price: 49.99,
+    },
+    {
+        name: "ELDEN RING",
+        price: 49.99,
+    },
+    {
+        name: "ELDEN RING",
+        price: 49.99,
+    },
+    {
+        name: "ELDEN RING NIGHTREIGN",
+        price: 19.99,
+    }
+];
+</script>
+
+<template>
+    <main class="container">
+        <h1>Wishlists</h1>
+        <CartItem v-for="game in games" :game></CartItem>
+    </main>
+</template>
