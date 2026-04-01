0f79878a9a7f34057d6198848ff19d4dba948e5a
GERRY Express <krittameth2020@gmail.com>
2026-04-01 14:31:32 +0700

    add cart item component

---
+++ src/components/CartItem.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+const {game} = defineProps(['game']);
+</script>
+
+<template>
+    <div class="item-container px-3 py-4 rounded-4 mb-3 row justify-content-between">
+        <img src="https://picsum.photos/350/200" class="rounded-4 col-4" alt="">
+        <div class="col-4 d-flex flex-column justify-content-start">            
+            <p class="fs-3 text-truncate mx-3 d-block">{{ game.name }}</p>
+            <p class="fs-4 text-truncate placeholder d-block">LDPAWLDPALW{D}</p>
+            <p class="fs-4 text-truncate d-block placeholder">DATE</p>
+        </div>
+        <div class=" col-4 row text-end">
+            <p class="fs-3">${{ game.price }}</p>
+            <a class="fs-4 link-danger align-self-end" href="">Remove</a>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.item-container {
+    background-color: #8B00F5;
+}
+</style>
