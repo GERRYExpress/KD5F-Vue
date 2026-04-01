0c2c9f003ea3bc4947d87190c2d5c733a04dd5c3
GERRY Express <krittameth2020@gmail.com>
2026-04-01 14:37:41 +0700

    add breadcrumb for cart and checkout page along with back to home button for every page except home

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,46 @@ import Navbar from './components/Navbar.vue';
 
 <template>
   <Navbar></Navbar>
-  <RouterView></RouterView>
+  <!-- Display back to home button if it's not homepage -->
+  <div v-if="$route.name !== 'Home'">
+    <div class="container">
+
+      <!-- Display breadcrumbs when the page is Cart and Checkout page -->
+      <div v-if="$route.name === 'Cart'">
+        <div id="divider" aria-label="breadcrumb">
+          <ol class="breadcrumb fs-1 fw-bold">
+            <li class="breadcrumb-item active" aria-current="page">Your Shopping Cart</li>
+            <li class="breadcrumb-item" aria-current="page">Checkout</li>
+          </ol>
+        </div>
+      </div>
+
+
+      <a id="back-to-home-button" href="/">< Back to Home</a>
+    </div>
+    <RouterView></RouterView>
+  </div>
+  <div v-else>
+    <RouterView></RouterView>
+  </div>
   <Footer></Footer>
 </template>
+
+<style scoped>
+#back-to-home-button {
+  color: #B7EF00;
+}
+
+/* Breadcrumbs styling in Cart and Checkout page */
+.active.breadcrumb-item {
+    color: #FEFEFE !important;
+    opacity: 1 !important;
+}
+.breadcrumb-item {
+  opacity: 0.5;
+}
+#divider {
+  --bs-breadcrumb-divider: '>';
+  --bs-breadcrumb-divider-color: #FEFEFE !important;
+}
+</style>
