790d55e291ebefedf82e86c65c8f6a3c120c0244
GERRY Express <krittameth2020@gmail.com>
2026-04-01 14:29:50 +0700

    add cart page with price calculation and show the added game

---
+++ src/views/Cart.vue
@@ -0,0 +1,90 @@
+<script lang="ts" setup>
+import CartItem from '../components/CartItem.vue';
+
+const games: Object[] = [
+    {
+        name: "DARK SOULS III",
+        price: 39.99,
+    },
+    {
+        name: "DARK SOULS REMASTERED",
+        price: 29.99,
+    },
+    {
+        name: "DARK SOULS REMASTERED",
+        price: 29.99,
+    },
+    {
+        name: "DARK SOULS REMASTERED",
+        price: 29.99,
+    },
+    {
+        name: "DARK SOULS II",
+        price: 39.99,
+    },
+    {
+        name: "DARK SOULS II",
+        price: 39.99,
+    },
+    {
+        name: "DARK SOULS II",
+        price: 39.99,
+    },
+    {
+        name: "ELDEN RING",
+        price: 49.99,
+    },
+    {
+        name: "ELDEN RING",
+        price: 49.99,
+    },
+    {
+        name: "ELDEN RING",
+        price: 49.99,
+    },
+    {
+        name: "ELDEN RING",
+        price: 49.99,
+    },
+    {
+        name: "ELDEN RING NIGHTREIGN",
+        price: 19.99,
+    }
+];
+const prices:number[] = games.map(game => game.price);
+let totalPrice: number = (prices.reduce((acc:number, val:number) => acc+val));
+const VAT:number = Number(totalPrice) * 0.07;
+totalPrice += VAT;
+totalPrice = Number(totalPrice.toFixed(2));
+
+</script>
+
+<template>
+    <main class="container d-flex justify-content-between align-items-stretch mx-auto my-3">
+        <div class="me-3">
+            <CartItem v-for="game in games" :game></CartItem>
+        </div>
+        <div class="summary-container rounded-4 ms-3 px-3 py-4 h-100">
+            <div class="row" v-for="game in games" :game>
+                <p class="col-6 text-start"> {{ game.name }}</p>
+                <p class="col-6 text-end">{{ game.price }}</p>
+            </div>
+            <hr>
+            <div class="row">
+                <p class="col-6 text-start">VAT</p>
+                <p class="col-6 text-end">{{ VAT.toFixed(2) }}</p>
+            </div>
+            <div class="row fs-3 fw-bold">
+                <p class="col-6 text-start">Total</p>
+                <p class="col-6 text-end">{{ totalPrice }}</p>
+            </div>
+            <a id="checkout-button" class="btn btn-primary rounded-pill fw-bold fs-2 w-100" href="/checkout">Checkout</a>
+        </div>
+    </main>
+</template>
+
+<style scoped>
+.summary-container {
+    background-color: #8B00F5;
+}
+</style>
