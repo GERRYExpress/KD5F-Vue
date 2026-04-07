d1b13b79890c17a4b9f2d89c37926deb38b5dc92
GERRY Express <krittameth2020@gmail.com>
2026-04-07 10:27:01 +0700

    route paths for Identification, Sign In, and Sign Up pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,9 @@ import Wishlist from '/src/views/Wishlist.vue';
 import Contact from '/src/views/Contact.vue';
 import Checkout from '/src/views/Checkout.vue';
 import Account from '../views/Account.vue';
+import SignUp from '../views/SignUp.vue';
+import Identification from '../views/Identification.vue';
+import SignIn from '../views/SignIn.vue';
 
 const routes = [
 
@@ -15,7 +18,10 @@ const routes = [
     { path: '/wishlist', name: 'Wishlist', component: Wishlist},
     { path: '/contact', name: 'Contact', component: Contact},
     { path: '/checkout', name: 'Checkout', component: Checkout},
-    { path: '/account', name: 'Account', component: Account}
+    { path: '/account', name: 'Account', component: Account},
+    { path: '/register', name: 'SignUp', component: SignUp},
+    { path: '/identification', name: 'Identification', component: Identification},
+    { path: '/login', name: 'login', component: SignIn}
 ]
 
 const router = createRouter( {
